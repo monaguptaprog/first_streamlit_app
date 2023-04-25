ae7de1ff98d10097f115adf9aa876d862bc3e605
monaguptaprog <131720044+monaguptaprog@users.noreply.github.com>
2023-04-25 11:16:49 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,3 +10,6 @@ streamlit.text('parantha')
 streamlit.text('🥑🍞 Pancake ')
 
 streamlit.header('Build your own smoothie')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
