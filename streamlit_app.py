1a891e6401bbcf651ea04cf953ff0bc66f1464e2
monaguptaprog <131720044+monaguptaprog@users.noreply.github.com>
2023-04-26 16:50:00 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,3 +26,8 @@ streamlit.dataframe(fruits_to_show)
 import requests
 fruitvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruitvice_response.json())
+
+# write your own comment -what does the next line do? 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
+streamlit.dataframe(fruityvice_normalized)
