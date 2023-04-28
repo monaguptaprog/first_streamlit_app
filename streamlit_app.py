8446482a9a3b5d2f66486ded6a0cf76cbdc8f23b
monaguptaprog <131720044+monaguptaprog@users.noreply.github.com>
2023-04-28 15:18:55 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,3 +44,7 @@ my_data_rows = my_cur.fetchall()
 #streamlit.text("The fruit load list contains:")
 #streamlit.text(my_data_row)
 streamlit.dataframe(my_data_rows)
+
+fruit_choice = streamlit.text_input('What fruit would you like to add?','jackfruit')
+streamlit.write('Thanks for adding ', fruit_choice)
+
